d3a254b124d6df9dde74ec742cc356a30711b556
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-31 10:09:39 +0530

    new file added

--- 02-convrsation.sh
+++ 02-convrsation.sh
@@ -5,4 +5,4 @@ person2=$2
 
 echo "$person1:Hi $Person2, How are you"
 echo "$person2:I am fine, How are you"
-echo :$person1:Great, Operation Sindhoor was successful"
+echo "$person1:Great, Operation Sindhoor was successful"
