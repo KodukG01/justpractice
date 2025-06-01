306a2c02ef08b83c13d9e3ce1e58928288beee7b
GautamKumar <kodukulagautamkumar@gmail.com>
2025-06-01 10:08:18 +0530

    new file added

--- 07-conditionals.sh
+++ 07-conditionals.sh
@@ -5,6 +5,16 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run the script with root access"
+    exit 1
 else
     echo "Runnning script with root access"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: MYSQL installation failed"
+    exit 1
+else
+    echo "MSYQL successfully installed"
