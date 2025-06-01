30828c416815cb978dd69db6b55d80e402a1b9aa
GautamKumar <kodukulagautamkumar@gmail.com>
2025-06-01 19:05:24 +0530

    new file added

---
+++ 09-for_loop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop_log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+packages=("mysql" "python" "nginx")
+
+mkdir -p $LOG_FOLDER
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROE:: Please run the script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else    
+    echo -e "Running script with root access" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$G Installing $2 is.....success $N" | tee -a $LOG_FILE
+    else
+        echo "$R Installing $2 is......failure $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+for packages in ${packages[@]}
+do
+    dnf list installed $packages $>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "Installing packages" | tee -a $LOG_FILE
+        dnf install $packages -y $>>$LOG_FILE
+    else
+        echo "Installed nothing to do" | tee -a $LOG_FILE
+    fi  
+done
