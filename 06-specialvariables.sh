7a826cf5323215a890038258e3b9e891de3f94bd
GautamKumar <kodukulagautamkumar@gmail.com>
2025-06-01 09:19:32 +0530

    new file added

---
+++ 06-specialvariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "Number of variables: $#"
+echo "Current script name: $0"
+echo "Current working directory: $PWD"
+echo "User running the script: $USER"
+echo "PID of current script: $$"
+echo "PID last command: $?"
