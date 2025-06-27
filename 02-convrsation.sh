fdda1e610404b400c6f15ffce4dfbbce7b4c11f1
GautamKumar <kodukulagautamkumar@gmail.com>
2025-06-27 12:59:23 +0530

    new

--- 02-convrsation.sh
+++ 02-convrsation.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-person1=$1
-person2=$2
+person1 = $1
+person2 = $2
 
-echo "$person1:Hi $Person2,How are you"
-echo "$person2:I am fine,How are you"
-echo "$person1:Great, Operation Sindhoor was successful"
+echo "$person1: Hi $person2, How are you"
+echo "$person2: Hi $person1, I am fine how are you"
