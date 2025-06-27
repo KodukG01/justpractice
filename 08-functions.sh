725bf48bb301c9d77fb6c7b9103aa62e5022fd40
GautamKumar <kodukulagautamkumar@gmail.com>
2025-06-27 15:51:42 +0530

    latest

--- 08-functions.sh
+++ 08-functions.sh
@@ -4,54 +4,50 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: Please run the script with root access"
-    exit 1
+    echo "Error:: Please run script with root access"
+exit 1
 else
-    echo "Running with root access"
+    echo "Script running with root access"
 fi
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo "Installing $2.....success"
+        echo "Installing $2 is........success"
     else
-        echo "Installing $2......failed"
-        exit 1
-    fi
+        echo "Installing $2 is........failure"
+    exit 1
 }
 
 dnf list installed mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installing MYSQL"
+    echo "installing mysql"
     dnf install mysql -y
     VALIDATE $? "mysql"
 else
-    echo "MYSQL installed nothing to do"
+    echo "Mysql installed nothing to do"
 fi
 
-dnf list installed python3
+dnf list installed python3 -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installing python3"
+    echo "installing python3"
     dnf install python3 -y
-    VALIDATE $? "Python3"
+    VALIDATE $? "python3"
 else
-    echo "Python3 already installed"
+    echo "python3 installed nothing to do"
 fi
 
-dnf list installed nginx
+dnf list installed mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installing nginx"
+    echo "installing nginx"
     dnf install nginx -y
     VALIDATE $? "nginx"
 else
-    echo "nginx already installed"
+    echo "nginx installed nothing to do"
 fi
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue.service"
