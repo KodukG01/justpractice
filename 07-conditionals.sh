73e390fb659a978a1b7511f631e6155787896df0
GautamKumar <kodukulagautamkumar@gmail.com>
2025-06-27 15:03:32 +0530

    latest

--- 07-conditionals.sh
+++ 07-conditionals.sh
@@ -4,26 +4,22 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run the script with root access"
-    exit 1
+echo "ERROR:: Please run with root access"
+exit 1
+
 else
-    echo "Runnning script with root access"
+echo "Script running with root access"
+
 fi
 
-dnf list installed mysql
+dnf list installed mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installing mysql"
-    dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "Mysql istalled successfully"
-    else
-        echo "Error: Failed to install mysql"
-        exit 1
-    fi
-else
-    echo "Mysql installed nothing to do"
+echo "Installing mysql -y"
 
+dnf install mysql -y
+
+else
+echo "Mysql installed successfully, nothing to do"
 fi
